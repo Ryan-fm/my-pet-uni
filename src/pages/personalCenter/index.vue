d2e692ebee3c8202c3e208faaddc47c1ba1acea8
Ryan <oncwnuAWivmLXaT4u-kwV72Dwlxk@git.weixin.qq.com>
2020-11-09 22:34:34 +0800

    项目初始化和基础菜单

---
+++ src/pages/personalCenter/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+      个人中心
+	</view>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+	export default Vue.extend({
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	});
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
